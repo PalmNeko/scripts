de020fc3027b73a64f63fce390e38f89a34e3a9f
PalmNeko <54460913+PalmNeko@users.noreply.github.com>
2024-06-11 22:25:03 +0900

    Update install_minilibx.sh

--- sh/install_minilibx.sh
+++ sh/install_minilibx.sh
@@ -8,6 +8,7 @@ which git || (echo you have to install git. like \`sudo apt install git-all\` ; 
 which apt || (echo you have to have 'apt' ; exit 1) || exit $? 
 
 # apt install some # I forgot its name.
+# when gcc is error, try it: apt install -y ca-certificates curl gnupg lsb-release
 apt install make gcc libx11-dev libxext-dev
 git clone https://github.com/42Paris/minilibx-linux.git minilibx
 cd minilibx
